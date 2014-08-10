5802c0836baa7160cf410c558e083b69868fc86d
Masahiro Atsum <raindrop.aqua@gmail.com>
2014-08-11 00:36:28 +0900

    part1 finished

--- django/mysite/polls/models.py
+++ django/mysite/polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
--- django/mysite/readme.md
+++ django/mysite/readme.md
@@ -6,8 +6,8 @@
 ## environment
 - python 2.7
 - django 1.6
+- sqlite 3
 
-## settings
-### database(sqlite3)
+## superuser account
 - user: admin
 - password: admin

c1e50ff9f015a7d2a85b5354c24ab4f8d102e426
Masahiro Atsum <raindrop.aqua@gmail.com>
2014-08-16 23:59:40 +0900

    step part3.

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+# coding: utf-8
+from django.http import HttpResponse
+from models import Poll
 
-# Create your views here.
+
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    output = ','.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
+
+
+def detail(request, poll_id):
+    return HttpResponse("You are looking at poll %s" % poll_id)
+
+
+def results(request, poll_id):
+    return HttpResponse("You are looking at the result of poll %s" % poll_id)
+
+
+def vote(request, poll_id):
+    return HttpResponse("You are voting on poll %s" % poll_id)
+
+
+
+
+
+

451846d726d99c8c314d52e52ece636b33b13d79
Masahiro Atsum <raindrop.aqua@gmail.com>
2014-08-17 22:18:36 +0900

    part3 finished.

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,16 +1,18 @@
 # coding: utf-8
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from models import Poll
 
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    output = ','.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    context = {'latest_poll_list': latest_poll_list, }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, poll_id):
-    return HttpResponse("You are looking at poll %s" % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 
 def results(request, poll_id):

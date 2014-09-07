64f0319f88b3f792e7d48a4981b80fe32bd4ec4b
Masahiro Atsum <raindrop.aqua@gmail.com>
2014-09-07 19:15:03 +0900

    add item search

--- web-service/amazon/Product Advertising API/python-amazon-product-api/item_search.py
+++ web-service/amazon/Product Advertising API/python-amazon-product-api/item_search.py
@@ -1,4 +1,25 @@
 # -*- coding: utf-8 -*-
 
+from amazonproduct.api import API
+from amazonproduct.errors import AWSError
+from amazonproduct.processors import BaseProcessor
+
+import BeautifulSoup
 
 
+class SoupProcessor(BaseProcessor):
+    def parse(self, fp):
+        soup = BeautifulSoup.BeautifulStoneSoup(fp.read())
+
+        for error in soup.findAll('error'):
+            code = error.find('code').text
+            msg = error.find('message').text
+            raise AWSError(code, msg)
+
+        return soup
+
+if __name__ == '__main__':
+    api = API(locale='jp', processor=SoupProcessor())
+    result = api.item_search('Apparel', Condition='All', Availability='Available', Keywords='Shirt')
+
+    print result
